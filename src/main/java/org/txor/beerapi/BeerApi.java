1819220e1f5d5fd3c7689c26785aeb3105bc9478
Txor <txorlings@gmail.com>
2021-02-27 11:11:37 +0100

    Spring initializr, .gitignore

---
+++ src/main/java/org/txor/beerapi/BeerApi.java
@@ -0,0 +1,13 @@
+package org.txor.beerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerApi {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerApi.class, args);
+	}
+
+}
